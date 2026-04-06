bff112bc54fd82ad286aced0152553d243fc58c2
Sumit Gupta <161856895+sumitgupta24@users.noreply.github.com>
2026-04-06 20:18:19 +0530

    Time: 2 ms (80.55%) | Memory: 13.6 MB (89.47%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void dfs(int node, int n, vector<vector<int>>& graph,vector<int>& temp, vector<vector<int>>& ans){
+        if(node == n - 1){
+            temp.push_back(node);
+            ans.push_back(temp);
+            temp.pop_back();
+            return;
+        }
+        temp.push_back(node);
+        for(auto& adjNode : graph[node]){
+            dfs(adjNode, n, graph, temp, ans);
+        }
+        temp.pop_back();
+    }
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> temp;
+        vector<vector<int>> ans;
+        dfs(0, n, graph, temp, ans);
+        return ans;
+    }
+};
