25c1b64ef24ee43a6b3954d09bba76c045c001fb
Sumit Gupta <161856895+sumitgupta24@users.noreply.github.com>
2026-04-06 11:08:30 +0530

    Time: 27 ms (53.33%) | Memory: 37.8 MB (56.53%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        int n = commands.size();
+        map<pair<int,int>, int> mpp;
+        for(auto& it: obstacles){
+            mpp[{it[0],it[1]}]++;
+        }
+        int maxPoint = 0;
+        int x = 0, y = 0;
+        int dir = 0;
+        for(int i=0;i<n;i++){
+            if(commands[i] == -2) dir = (dir + 1) % 4;
+            else if(commands[i] == -1) dir = ((dir - 1) % 4 + 4) % 4;
+            else{
+                int k = commands[i];
+                if(dir == 0){
+                    for(int j=0;j<k;j++){
+                        if(!mpp.count({x, y + 1})) y++;
+                    }
+                }
+                else if(dir == 1){
+                    for(int j=0;j<k;j++){
+                        if(!mpp.count({x - 1, y})) x--;
+                    }
+                }
+                else if(dir == 2){
+                    for(int j=0;j<k;j++){
+                        if(!mpp.count({x, y - 1})) y--;
+                    }
+                }
+                else if(dir == 3){
+                    for(int j=0;j<k;j++){
+                        if(!mpp.count({x + 1, y})) x++;
+                    }
+                }
+            }
+            maxPoint = max(maxPoint, x * x + y * y);
+        }
+        return maxPoint;
+    }
+};
