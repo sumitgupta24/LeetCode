e7a3d737ad8b08a0370ffc6e9a87726c41cc99b7
Sumit Gupta <161856895+sumitgupta24@users.noreply.github.com>
2026-04-26 11:25:36 +0530

    Time: 384 ms (15.22%) | Memory: 99.9 MB (35.25%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    bool dfs(int r,int c,int pr,int pc,vector<vector<char>>&grid,vector<vector<int>>&vis){
+    bool dfs(int r, int c, int pr, int pc, vector<vector<char>>& grid, vector<vector<int>>& vis) {
         int m = grid.size();
         int n = grid[0].size();
 
         vis[r][c] = 1;
-        int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+        int dir[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
 
-        for(auto &d : dir){
+        for(auto &d : dir) {
             int nr = r + d[0];
             int nc = c + d[1];
 
-            if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc] == grid[r][c]){
-                if(nr==pr && nc==pc) continue;
+            if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == grid[r][c]) {
+                if(nr == pr && nc == pc) continue;
                 if(vis[nr][nc]) return true;
-                if(dfs(nr,nc,r,c,grid,vis)) return true;
+                if(dfs(nr, nc, r, c, grid, vis)) return true;
             }
         }
         return false;
@@ -24,12 +24,12 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> vis(m,vector<int>(n,0));
+        vector<vector<int>> vis(m, vector<int>(n, 0));
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!vis[i][j]){
-                    if(dfs(i,j,-1,-1,grid,vis)){
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!vis[i][j]) {
+                    if(dfs(i, j, -1, -1, grid, vis)){
                         return true;
                     }
                 }
