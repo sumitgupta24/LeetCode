623ad49a6df27dd06a329005cbced4720477fbb1
Sumit Gupta <161856895+sumitgupta24@users.noreply.github.com>
2026-05-09 19:10:40 +0545

    Time: 21 ms (11.76%) | Memory: 17.9 MB (42.65%) - LeetSync

---
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void fillAgain(int row, int col, vector<vector<int>>& grid, vector<int>& arr) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int p = 0;
+
+        for(int i = row; i < n - row; i++) grid[i][col] = arr[p++];
+        for(int j = col + 1; j < m - col; j++) grid[n - row - 1][j] = arr[p++];
+        for(int i = n - row - 2; i >= row; i--) grid[i][m - col - 1] = arr[p++];
+        for(int j = m - col - 2; j > col; j--) grid[row][j] = arr[p++];
+
+    }
+
+    vector<int> getArray(int row, int col, vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<int> arr;
+
+        for(int i = row; i < n - row; i++) arr.push_back(grid[i][col]);
+        for(int j = col + 1; j < m - col; j++) arr.push_back(grid[n - row - 1][j]);
+        for(int i = n - row - 2; i >= row; i--) arr.push_back(grid[i][m - col - 1]);
+        for(int j = m - col - 2; j > col; j--) arr.push_back(grid[row][j]);
+
+        return arr;
+    }
+
+    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i = 0; i < min(n, m) / 2; i++) {
+            vector<int> arr = getArray(i, i, grid);
+            int size = arr.size();
+            int temp = k;
+            temp = temp % size;
+            // for(auto& it : arr) {
+            //     cout << it << " ";
+            // }
+            // cout << endl;
+            reverse(arr.begin(), arr.end());
+            reverse(arr.begin(), arr.begin() + temp);
+            reverse(arr.begin() + temp, arr.end());
+            // for(auto& it : arr) {
+            //     cout << it << " ";
+            // }
+            // cout << endl;
+            fillAgain(i, i, grid, arr);
+        }
+        
+        return grid;
+    }
+};
