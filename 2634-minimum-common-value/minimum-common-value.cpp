44555c357f5e32140cd0735cbbd28c3d15453ffe
Sumit Gupta <161856895+sumitgupta24@users.noreply.github.com>
2026-05-19 11:52:57 +0545

    Time: 46 ms (13.84%) | Memory: 69.6 MB (17.77%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -3,7 +3,6 @@ public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
         unordered_set<int> st;
         for(int i = 0; i < n; i++) {
