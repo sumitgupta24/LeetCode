2a987d4c5c89c9014f598dc7802bdb21d9bc70b9
Sumit Gupta <161856895+sumitgupta24@users.noreply.github.com>
2026-06-14 19:23:01 +0545

    Time: 1 ms (99.87%) | Memory: 46.7 MB (65.40%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -6,7 +6,7 @@ class Solution {
             if(i % 15 == 0) ans.add("FizzBuzz");
             else if(i % 3 == 0) ans.add("Fizz");
             else if(i % 5 == 0) ans.add("Buzz");
-            else ans.add(String.valueOf(i));
+            else ans.add(Integer.toString(i));
         }
 
         return ans;
