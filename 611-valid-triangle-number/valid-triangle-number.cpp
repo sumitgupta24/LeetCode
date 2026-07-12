d1c98e6f35953df8ecb32d6881aae60f144b3b63
Sumit Gupta <161856895+sumitgupta24@users.noreply.github.com>
2026-07-12 17:22:34 +0530

    Time: 24 ms (95.69%) | Memory: 16.6 MB (45.71%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -7,11 +7,16 @@ public:
 
         int count = 0;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                for(int k = j + 1; k < n; k++) {
-                    if(nums[i] + nums[j] > nums[k]) count++;
+        for(int k = n - 1; k > 1; k--) {
+            int i = 0;
+            int j = k - 1;
+
+            while(i < j) {
+                if(nums[i] + nums[j] > nums[k]) {
+                    count += j - i;
+                    j--;
                 }
+                else i++;
             }
         }
 
