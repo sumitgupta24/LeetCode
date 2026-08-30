cbb83f4f82e8087928311f334f950d274e5f65da
Sumit Gupta <161856895+sumitgupta24@users.noreply.github.com>
2026-08-30 20:36:45 +0530

    Time: 3 ms (36.81%) | Memory: 92.1 MB (58.82%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -2,27 +2,19 @@ class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
         int n = nums.size();
-        pair<int, int> maxi, mini;
-
-        maxi.first = INT_MIN;
-        mini.first = INT_MAX;
+        
+        int minIndex = 0;
+        int maxIndex = 0;
 
         for(int i = 0; i < n; i++) {
-            if(nums[i] > maxi.first) {
-                maxi.first = nums[i];
-                maxi.second = i;
-            }
-
-            if(nums[i] < mini.first) {
-                mini.first = nums[i];
-                mini.second = i;
-            }
+            if(nums[i] > nums[maxIndex]) maxIndex = i;
+            if(nums[i] < nums[minIndex]) minIndex = i;
         }
 
-        int case1 = max(mini.second, maxi.second) + 1;
-        int case2 = max(n - mini.second, n - maxi.second);
-        int case3 = mini.second + n - maxi.second + 1;
-        int case4 = maxi.second + n - mini.second + 1;
+        int case1 = max(maxIndex, minIndex) + 1;
+        int case2 = max(n - minIndex, n - maxIndex);
+        int case3 = minIndex + 1 + n - maxIndex;
+        int case4 = maxIndex + 1 + n - minIndex;
 
         return min({case1, case2, case3, case4});
     }
